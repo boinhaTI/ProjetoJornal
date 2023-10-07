f3758d924c7e678aa8ec855b865cec3e96de83fb
boinhaTI <grafboinha@gmail.com>
2023-10-07 17:37:15 -0300

    PROJETO JORNAL

---
+++ Menu.py
@@ -0,0 +1,17 @@
+print(20 * '<=>', 'NOTICIAS QUENTES', 20 * '<=>')
+
+
+
+print('>>>Menú Principal<<<\n\n1-CADASTRO ADM\n2-CADASTRO USUÁRIO\n3-NOTICIAS FAVORITAS\n'
+      '4-')
+menu = int(input('Digite a sua opção: '))
+
+if menu == 1:
+      print('PREENCHA O CADASTRO ADM')
+      print()
+      nome = input('Nome Completo: ')
+      CPF = int(input('Nº do CPF: '))
+      DataNac = int(input('Digite o sua data nascimento: '))
+      Login = input('Digite')
+elif menu == 2:
+
---
+++ cod.py
@@ -0,0 +1,27 @@
+usuario = []
+
+op = 99
+
+while(op != 0):
+    print('1- CADASTRAR USUÁRIO ADMIN:')
+    print('2- CADASTRAR USUÁRIO LEITOR:')
+    print('3- EFETUAR O LOGIN:')
+    print('4- PUBLICAR NOTÍCIA:')
+    print('5- SAIR:')
+    input('escolha uma opção:')
+
+    if(op == 1):
+        nome = input('Nome Completo: ')
+        login = input('Nome Usuário: ')
+        senha = input('Senha: ')
+        DataN = input('Data Nascimento: ')
+    elif(op == 2):
+        nome = input('Nome Completo: ')
+        login = input('Nome Usuário: ')
+        senha = input('Senha: ')
+        DataN = input('Data Nascimento: ')
+    elif (op == 5):
+        nome = input('Nome Completo: ')
+        login = input('Nome Usuário: ')
+        senha = input('Senha: ')
+        DataN = input('Data Nascimento: ')
